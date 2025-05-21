9e42d1bef2fe1c4b94ff8c694a120f63c9c57df5
Lindsay Evans <lindsay.evans@boomworks.com.au>
2025-05-21 11:12:25 +1000

    test: add test for scope resolution

--- jlox/test.sh
+++ jlox/test.sh
@@ -6,3 +6,4 @@ assert "./jlox tests/while.lox" equals "1"$'\n'"2"$'\n'"3"$'\n'
 assert "./jlox tests/for.lox" equals "0"$'\n'"1"$'\n'"2"$'\n'
 # assert "./jlox tests/clock.lox" exits 1
 assert "./jlox tests/fun.lox" equals "6"$'\n'
+assert "./jlox tests/scope_resolution.lox" equals "global"$'\n'"block"$'\n'"global"$'\n'
