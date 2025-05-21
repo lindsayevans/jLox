dc9773a921c9cc6653c4b3d0a1128d64f7408c35
Lindsay Evans <lindsay.evans@boomworks.com.au>
2025-05-22 07:10:23 +1000

    feat: initial VM implementation
    https://craftinginterpreters.com/a-virtual-machine.html

--- clox/src/main.c
+++ clox/src/main.c
@@ -2,11 +2,14 @@
 #include <stdio.h>
 
 #include "common.h"
+#include "vm.h"
 #include "chunk.h"
 #include "debug.h"
 
 int main(int argc, char **argv)
 {
+    initVM();
+
     Chunk chunk;
     initChunk(&chunk);
 
@@ -14,9 +17,24 @@ int main(int argc, char **argv)
     writeChunk(&chunk, OP_CONSTANT, 123);
     writeChunk(&chunk, constant, 123);
 
+    constant = addConstant(&chunk, 3.4);
+    writeChunk(&chunk, OP_CONSTANT, 123);
+    writeChunk(&chunk, constant, 123);
+
+    writeChunk(&chunk, OP_ADD, 123);
+
+    constant = addConstant(&chunk, 5.6);
+    writeChunk(&chunk, OP_CONSTANT, 123);
+    writeChunk(&chunk, constant, 123);
+
+    writeChunk(&chunk, OP_DIVIDE, 123);
+
+    writeChunk(&chunk, OP_NEGATE, 123);
+
     writeChunk(&chunk, OP_RETURN, 123);
 
     disassembleChunk(&chunk, "test chunk");
+    interpret(&chunk);
 
     freeChunk(&chunk);
 
