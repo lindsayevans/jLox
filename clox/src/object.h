d129876c6d35a665609b45aefc73f19c7649dd4e
Lindsay Evans <lindsay.evans@boomworks.com.au>
2025-05-26 15:40:34 +1000

    feat: implement calls & functions
    https://craftinginterpreters.com/calls-and-functions.html

--- clox/src/object.h
+++ clox/src/object.h
@@ -2,16 +2,26 @@
 #define clox_object_h
 
 #include "common.h"
+#include "chunk.h"
 #include "value.h"
 
 #define OBJ_TYPE(value) (AS_OBJ(value)->type)
-#define IS_STRING(value) isObjType(value, OBJ_STRING)
 
+#define IS_FUNCTION(value) isObjType(value, OBJ_FUNCTION)
+#define AS_FUNCTION(value) ((ObjFunction *)AS_OBJ(value))
+
+#define IS_NATIVE(value) isObjType(value, OBJ_NATIVE)
+#define AS_NATIVE(value) \
+    (((ObjNative *)AS_OBJ(value))->function)
+
+#define IS_STRING(value) isObjType(value, OBJ_STRING)
 #define AS_STRING(value) ((ObjString *)AS_OBJ(value))
 #define AS_CSTRING(value) (((ObjString *)AS_OBJ(value))->chars)
 
 typedef enum
 {
+    OBJ_FUNCTION,
+    OBJ_NATIVE,
     OBJ_STRING,
 } ObjType;
 
@@ -21,6 +31,22 @@ struct Obj
     struct Obj *next;
 };
 
+typedef struct
+{
+    Obj obj;
+    int arity;
+    Chunk chunk;
+    ObjString *name;
+} ObjFunction;
+
+typedef Value (*NativeFn)(int argCount, Value *args);
+
+typedef struct
+{
+    Obj obj;
+    NativeFn function;
+} ObjNative;
+
 struct ObjString
 {
     Obj obj;
@@ -28,6 +54,9 @@ struct ObjString
     char *chars;
     uint32_t hash;
 };
+
+ObjFunction *newFunction();
+ObjNative *newNative(NativeFn function);
 
 ObjString *takeString(char *chars, int length);
 ObjString *copyString(const char *chars, int length);
